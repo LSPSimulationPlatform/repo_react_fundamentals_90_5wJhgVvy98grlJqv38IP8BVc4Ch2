65510ccd6debee25ca785104e8a35f50d69c8d2b
RasulAhmadov <rasul.ahmadov.56@gmail.com>
2025-08-26 10:38:28 +0000

    My first project commited

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+  <div>
+<h1> Hello world!</h1>
+<label style={{ fontSize:"30px",color:"red"}}> It's my first project. </label>
+</div>
+
+  )
+}
+
+export default App;
